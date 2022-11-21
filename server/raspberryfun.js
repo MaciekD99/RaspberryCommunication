279d3b225238269e7131510f29194b1f15b8d563
maciek <maciejdysput@gmail.com>
2022-11-21 19:20:55 +0100

    sequential execution of commands

--- server/raspberryfun.js
+++ server/raspberryfun.js
@@ -1,23 +1,38 @@
 
-// function oneByOne(){
-//   setTimeout( flowingLeds,0)
-//   setTimeout( servo, 5000)
-//   setTimeout(blinkRed,25000)
-//   setTimeout(blinkGreen,30000)
-//   setTimeout(blinkYellow,35000)
-//   setTimeout(tryRed,40000)
-//   setTimeout(endBlinkRed,45000)
-//   setTimeout(tryGreen,45000)
-//   setTimeout(endBlinkGreen,50000)
-//   setTimeout(tryYellow,50000)
-//   setTimeout(endBlinkYellow,55000)
-//   setTimeout(sensor, 55000)
-  
-// }
+function servo(){
+const Gpio = require('pigpio').Gpio;
+
+const motor = new Gpio(10, {mode: Gpio.OUTPUT});
+
+
+return new Promise((resolve) => { 
+  var servoInterval = setInterval(runServo, 1000);
+  let pulseWidth = 1000;
+let increment = 100;
+function runServo(){
+motor.servoWrite(pulseWidth);
+
+  pulseWidth += increment;
+  if (pulseWidth >= 2000) {
+    increment = -100;
+  } else if (pulseWidth <= 1000) {
+    increment = 100;
+  }
+}
+setTimeout(() => {
+  clearInterval(servoInterval);
+  resolve()
+}, 20000)
+})
+
+
+}
+
 function blinkRed(){
   var Gpio = require('onoff').Gpio; 
   var LED = new Gpio(4, 'out'); 
-  var blinkInterval = setInterval(blinkLED, 250); 
+  return new Promise((resolve) => { 
+    var blinkInterval = setInterval(blinkLED, 250); 
 function blinkLED() { 
   if (LED.readSync() === 0) { 
     LED.writeSync(1); 
@@ -26,12 +41,56 @@ function blinkLED() {
     LED.writeSync(0); 
   }
 }
-  function endBlink() { 
-  clearInterval(blinkInterval); 
+setTimeout(() => {
+clearInterval(blinkInterval); 
   LED.writeSync(0); 
   LED.unexport(); 
+  resolve()
+},5000)
+  })
 }
-setTimeout(endBlink, 5000); 
+
+function blinkGreen(){
+  var Gpio = require('onoff').Gpio; 
+  var LED = new Gpio(17, 'out'); 
+  return new Promise((resolve) => { 
+    var blinkInterval = setInterval(blinkLED, 250); 
+function blinkLED() { 
+  if (LED.readSync() === 0) { 
+    LED.writeSync(1); 
+    
+  } else {
+    LED.writeSync(0); 
+  }
+}
+setTimeout(() => {
+clearInterval(blinkInterval); 
+  LED.writeSync(0); 
+  LED.unexport(); 
+  resolve()
+},5000)
+  }) 
+}
+function blinkYellow(){
+  var Gpio = require('onoff').Gpio; 
+  var LED = new Gpio(27, 'out'); 
+  return new Promise((resolve) => { 
+    var blinkInterval = setInterval(blinkLED, 250); 
+function blinkLED() { 
+  if (LED.readSync() === 0) { 
+    LED.writeSync(1); 
+    
+  } else {
+    LED.writeSync(0); 
+  }
+}
+setTimeout(() => {
+clearInterval(blinkInterval); 
+  LED.writeSync(0); 
+  LED.unexport(); 
+  resolve()
+},5000)
+  }) 
 }
 
 function tryRed(){
@@ -99,6 +158,36 @@ function endBlinkYellow() {
 },3000)
   })}
 
+  function flowingLeds(){
+    var Gpio = require('onoff').Gpio; 
+var LED04 = new Gpio(4, 'out'), 
+  LED17 = new Gpio(17, 'out'),
+  LED27 = new Gpio(27, 'out');
+  var leds = [LED04, LED17, LED27];
+  var indexCount = 0; 
+dir = "up"; 
+return new Promise((resolve) => {
+  var flowInterval = setInterval(flowingLeds, 300); 
+    function flowingLeds() { 
+  leds.forEach(function(currentValue) { 
+    currentValue.writeSync(0); 
+  });
+  if (indexCount == 0) dir = "up"; 
+  if (indexCount >= leds.length) dir = "down"; 
+  if (dir == "down") indexCount--; 
+  leds[indexCount].writeSync(1); 
+  if (dir == "up") indexCount++ 
+};
+  setTimeout(() => {
+  clearInterval(flowInterval); 
+  leds.forEach(function(currentValue) {
+    currentValue.writeSync(0); 
+    currentValue.unexport(); 
+    resolve()
+  });
+},5000)
+}) }
+
 async function oneByOne() {
   await tryRed();
   await endBlinkRed();
@@ -106,6 +195,11 @@ async function oneByOne() {
   await endBlinkGreen();
   await tryYellow();
   await endBlinkYellow();
+  await flowingLeds();
+  await blinkRed();
+  await blinkGreen();
+  await blinkYellow();
+  await servo();
 }
 // function tryRed(){
 //   var Gpio = require('onoff').Gpio; 
@@ -126,25 +220,25 @@ async function oneByOne() {
 
 
 
-function blinkGreen(){
-  var Gpio = require('onoff').Gpio; 
-  var LED = new Gpio(17, 'out'); 
-  var blinkInterval = setInterval(blinkLED, 250); 
-function blinkLED() { 
-  if (LED.readSync() === 0) { 
-    LED.writeSync(1); 
+// function blinkGreen(){
+//   var Gpio = require('onoff').Gpio; 
+//   var LED = new Gpio(17, 'out'); 
+//   var blinkInterval = setInterval(blinkLED, 250); 
+// function blinkLED() { 
+//   if (LED.readSync() === 0) { 
+//     LED.writeSync(1); 
     
-  } else {
-    LED.writeSync(0); 
-  }
-}
-  function endBlink() { 
-  clearInterval(blinkInterval); 
-  LED.writeSync(0); 
-  LED.unexport(); 
-}
-setTimeout(endBlink, 5000); 
-}
+//   } else {
+//     LED.writeSync(0); 
+//   }
+// }
+//   function endBlink() { 
+//   clearInterval(blinkInterval); 
+//   LED.writeSync(0); 
+//   LED.unexport(); 
+// }
+// setTimeout(endBlink, 5000); 
+// }
 // function tryGreen(){
 //   var Gpio = require('onoff').Gpio; 
 //   var LED = new Gpio(17, 'out'); 
@@ -160,25 +254,25 @@ setTimeout(endBlink, 5000);
 //  }
 
 
-function blinkYellow(){
-  var Gpio = require('onoff').Gpio; 
-  var LED = new Gpio(27, 'out'); 
-  var blinkInterval = setInterval(blinkLED, 250); 
-function blinkLED() { 
-  if (LED.readSync() === 0) { 
-    LED.writeSync(1); 
+// function blinkYellow(){
+//   var Gpio = require('onoff').Gpio; 
+//   var LED = new Gpio(27, 'out'); 
+//   var blinkInterval = setInterval(blinkLED, 250); 
+// function blinkLED() { 
+//   if (LED.readSync() === 0) { 
+//     LED.writeSync(1); 
     
-  } else {
-    LED.writeSync(0); 
-  }
-}
-  function endBlink() { 
-  clearInterval(blinkInterval); 
-  LED.writeSync(0); 
-  LED.unexport(); 
-}
-setTimeout(endBlink, 5000); 
-}
+//   } else {
+//     LED.writeSync(0); 
+//   }
+// }
+//   function endBlink() { 
+//   clearInterval(blinkInterval); 
+//   LED.writeSync(0); 
+//   LED.unexport(); 
+// }
+// setTimeout(endBlink, 5000); 
+// }
 // function tryYellow(){
 //   var Gpio = require('onoff').Gpio; 
 //   var LED = new Gpio(27, 'out'); 
@@ -196,46 +290,46 @@ setTimeout(endBlink, 5000);
 
 
 //  flowing leds //
-function flowingLeds(){
-    var Gpio = require('onoff').Gpio; 
-var LED04 = new Gpio(4, 'out'), 
-  LED17 = new Gpio(17, 'out'),
-  LED27 = new Gpio(27, 'out');
-  var leds = [LED04, LED17, LED27];
-  var indexCount = 0; 
-dir = "up"; 
+// function flowingLeds(){
+//     var Gpio = require('onoff').Gpio; 
+// var LED04 = new Gpio(4, 'out'), 
+//   LED17 = new Gpio(17, 'out'),
+//   LED27 = new Gpio(27, 'out');
+//   var leds = [LED04, LED17, LED27];
+//   var indexCount = 0; 
+// dir = "up"; 
 
-var flowInterval = setInterval(flowingLeds, 300); 
+// var flowInterval = setInterval(flowingLeds, 300); 
 
-function flowingLeds() { 
-  leds.forEach(function(currentValue) { 
-    currentValue.writeSync(0); 
-  });
-  if (indexCount == 0) dir = "up"; 
-  if (indexCount >= leds.length) dir = "down"; 
-  if (dir == "down") indexCount--; 
-  leds[indexCount].writeSync(1); 
-  if (dir == "up") indexCount++ 
-};
+// function flowingLeds() { 
+//   leds.forEach(function(currentValue) { 
+//     currentValue.writeSync(0); 
+//   });
+//   if (indexCount == 0) dir = "up"; 
+//   if (indexCount >= leds.length) dir = "down"; 
+//   if (dir == "down") indexCount--; 
+//   leds[indexCount].writeSync(1); 
+//   if (dir == "up") indexCount++ 
+// };
 
-function unexportOnClose() {
-  clearInterval(flowInterval); 
-  leds.forEach(function(currentValue) {
-    currentValue.writeSync(0); 
-    currentValue.unexport(); 
-  });
-};
-process.on('SIGINT', unexportOnClose);
+// function unexportOnClose() {
+//   clearInterval(flowInterval); 
+//   leds.forEach(function(currentValue) {
+//     currentValue.writeSync(0); 
+//     currentValue.unexport(); 
+//   });
+// };
+// process.on('SIGINT', unexportOnClose);
 
-  function endBlink() { 
- clearInterval(flowInterval); 
-  leds.forEach(function(currentValue) { 
-    currentValue.writeSync(0); 
-    currentValue.unexport(); 
-  });
-}
-setTimeout(endBlink, 5000); 
-}
+//   function endBlink() { 
+//  clearInterval(flowInterval); 
+//   leds.forEach(function(currentValue) { 
+//     currentValue.writeSync(0); 
+//     currentValue.unexport(); 
+//   });
+// }
+// setTimeout(endBlink, 5000); 
+// }
 
 
                                 /// Czujnik ///
@@ -281,32 +375,32 @@ setTimeout(() => {
 
 }
 
-function servo(){
-const Gpio = require('pigpio').Gpio;
+// function servo(){
+// const Gpio = require('pigpio').Gpio;
 
-const motor = new Gpio(10, {mode: Gpio.OUTPUT});
+// const motor = new Gpio(10, {mode: Gpio.OUTPUT});
 
-let pulseWidth = 1000;
-let increment = 100;
+// let pulseWidth = 1000;
+// let increment = 100;
 
-const end = setInterval(() => {
-  motor.servoWrite(pulseWidth);
+// const end = setInterval(() => {
+//   motor.servoWrite(pulseWidth);
 
-  pulseWidth += increment;
-  if (pulseWidth >= 2000) {
-    increment = -100;
-  } else if (pulseWidth <= 1000) {
-    increment = 100;
-  }
+//   pulseWidth += increment;
+//   if (pulseWidth >= 2000) {
+//     increment = -100;
+//   } else if (pulseWidth <= 1000) {
+//     increment = 100;
+//   }
   
-  console.log('start')
-}, 1000);
+//   console.log('start')
+// }, 1000);
 
-setTimeout(() => {
-  clearInterval(end);
-}, 20000)
+// setTimeout(() => {
+//   clearInterval(end);
+// }, 20000)
 
-}
+// }
 
 
 module.exports = {oneByOne, endBlinkRed,blinkRed,blinkGreen,tryGreen,endBlinkGreen,tryRed, blinkYellow,tryYellow,endBlinkYellow,servo,sensor, flowingLeds };
